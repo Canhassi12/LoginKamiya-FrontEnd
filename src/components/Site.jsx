a854e64620ecf272b08504b65060fa0f7347e781
Canhassi <arthurcanhassi@gmail.com>
2022-08-20 16:42:44 -0300

    fix: responsive design

--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -1,7 +1,7 @@
 export default function Site() {
     return (
         <div> 
-            <img className="w-[550px] z-20" src="src\assets\Captura de tela 2022-08-19 165823.png"/> 
+            <img className="sm:w-[550px] w-[300px]" src="src\assets\Captura de tela 2022-08-19 165823.png"/> 
         </div> 
     );
 }
