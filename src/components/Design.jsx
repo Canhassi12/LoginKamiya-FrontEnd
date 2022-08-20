eceaa73a43970c8de08c84df52045caa19ffc1d4
Canhassi <arthurcanhassi@gmail.com>
2022-08-20 16:42:09 -0300

    fix: responsive design

--- src/components/Design.jsx
+++ src/components/Design.jsx
@@ -1,7 +1,7 @@
 export default function Kamiya1() {
     return (
         <div> 
-            <img className="w-[550px] z-20" src="src\assets\Sem título (3).png"/> 
+            <img className="sm:w-[550px] w-[300px] " src="src\assets\Sem título (3).png"/> 
         </div> 
     );
 }
